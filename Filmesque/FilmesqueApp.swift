79c520716d1689824f4c5f139520900a61498c2f
Tom Yan <143831712+tomyanzhiyuan@users.noreply.github.com>
2025-03-10 17:39:12 -0400

    Initial Commit

---
+++ Filmesque/FilmesqueApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmesqueApp.swift
+//  Filmesque
+//
+//  Created by Tom Yan Zhiyuan on 10/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct FilmesqueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
